6e07abe73a8c8b3a7c0e7afc869ec520b9465407
MaxZarn <m.zarn@mail.ru>
2019-05-27 20:02:07 +0300

    fix task 3

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -2,39 +2,15 @@
 
 const dead = document.getElementById('dead');
 const lost = document.getElementById('lost');
-const mole = document.getElementById('hole_has-mole');
-//const hole = document.getElementsByClassName('hole');
+const mole = document.getElementsByClassName('hole_has-mole');
+const hole = document.getElementsByClassName('hole');
 
-
-function getHole(index) {
-    return document.getElementById(`hole${index}`)
-};
-
-const hole = getHole(index);
-
-for (let i = 0; i < 9; i++) {
-    emptyHole = hole[i];
-    emptyHole.onclick = function() {
-        if (hole.className.includes( 'hole_has-mole' )) {
-            dead.textContent = + dead.textContent + 1;
+for (let i = 0; i < hole.length; i++) {
+    hole[i].onclick = function() {
+        if (hole[i].className.includes( 'hole_has-mole' ) === true) {
+           dead.textContent = +dead.textContent + 1;
         } else {
-            lost.textContent = + lost.textContent + 1;
+          lost.textContent = +lost.textContent + 1;
         }
-    }
-}
-
-/*
-hole.onclick = function() {
-    if (hole.className.includes( 'hole_has-mole' )) {
-        dead.textContent = + dead.textContent + 1;
-    } else {
-        lost.textContent = + lost.textContent + 1;
-    }
-}*/
-
-mole.onclick = function() {
-        dead.textContent = +dead.textContent + 1;
-    
-        lost.textContent = +lost.textContent + 1;
-    
-}
+    };
+};
