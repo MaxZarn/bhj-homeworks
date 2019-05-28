a83159c4c5e3c2347f9d53d30cb20c3a23aa8a57
MaxZarn <m.zarn@mail.ru>
2019-05-28 23:16:40 +0300

    added reset stat

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -9,8 +9,18 @@ for (let i = 0; i < hole.length; i++) {
     hole[i].onclick = function() {
         if (hole[i].className.includes( 'hole_has-mole' ) === true) {
            dead.textContent = +dead.textContent + 1;
+           if (dead.textContent > 9) {
+            alert('Вы победили!');
+            dead.textContent = 0;
+            lost.textContent = 0;
+          };
         } else {
           lost.textContent = +lost.textContent + 1;
+          if (lost.textContent > 4) {
+            alert('Вы проиграли!');
+            dead.textContent = 0;
+            lost.textContent = 0;
+          }
         }
     };
 };
